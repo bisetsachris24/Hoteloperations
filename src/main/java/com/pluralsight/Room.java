f55686cfec71f8fd38c121bc80351fec4facb278
amanichristianbisetsa <bisetsachristian@gmail.com>
2026-05-04 14:55:20 -0500

    3rd commit,

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -9,10 +9,47 @@ public class Room {
 
 
     // I am about to create constructor
+    public Room (int numberOfBeds, double price, boolean isOccupied, boolean isDirty ) {
 
-    public Room (int numberOfBeds,double price, boolean isOccupied, boolean isDirty ){
 
-
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isOccupied = isOccupied;
+        this.isDirty = isDirty;
     }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        isDirty = dirty;
+    }
+
+
 
 }
